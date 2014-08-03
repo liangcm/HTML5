4d97ca841b30bebcd4ce1b90f03579113d6704cb
liangcm <ray@raydeMacBook-Pro.local>
2014-08-03 22:02:25 +0800

    draw something.

--- HelloWorld/HelloWorld.js
+++ HelloWorld/HelloWorld.js
@@ -5,5 +5,20 @@ context.fillStyle='cornflowerblue';
 context.strokeStyle='blue';
 context.fillText('Hello World',canvas.width/2 - 150,
 canvas.hegiht/2 + 15);
+// how to drow & stroke text
 context.strokeText('Hello World', canvas.width/2 - 150,
 canvas.height/2 + 15);
+
+// how to fill a rectangle.
+context.fillStyle='#ff0000';
+context.fillRect(0,0, 100, 100);
+
+// how to strok a circle.
+context.beginPath();
+context.arc(canvas.width/2, canvas.height/2, 100, 0, Math.PI*2, true);
+context.strokeStyle='red'
+context.stroke();
+
+// how to alert sth.
+alert('Hello World!');
+alert('another alert');
